2fa376336cadfff40c5e5b5aca5cea87e2ff8111
Krishnavamsi Bendalam <bendalamkrishnavamsi@gmail.com>
2024-03-05 11:09:43 +0530

    build erros resolution

--- src/Videos.js
+++ src/Videos.js
@@ -40,7 +40,7 @@ function Videos() {
 }
 
 function getYouTubeID(url) {
-  const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+  const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
   const match = url.match(regExp);
 
   if (match && match[2].length === 11) {
